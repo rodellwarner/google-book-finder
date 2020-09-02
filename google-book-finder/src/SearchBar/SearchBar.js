255623d6e1a0641c6558fb37c86ac2b845897b51
Rodell Warner <rodell.warner@gmail.com>
2020-09-02 14:50:28 -0500

    SearchBar fetch requet working

--- google-book-finder/src/SearchBar/SearchBar.js
+++ google-book-finder/src/SearchBar/SearchBar.js
@@ -2,10 +2,44 @@ import React, { Component } from "react";
 import "./SearchBar.css";
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+    };
+  }
+
+  searchTermChanged(newSearchTerm) {
+    this.setState({
+      searchTerm: newSearchTerm,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const url =
+      "https://www.googleapis.com/books/v1/volumes?q=" +
+      `${this.state.searchTerm}`;
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: "AIzaSyBdDX-dj38R1mz5nYD3eCLNTjzp2g90o8E",
+        "Content-type": "application/json",
+      },
+    };
+
+    console.log("URL: ", url);
+
+    fetch(url, options)
+      .then((results) => results.json())
+      .then((resultsJSON) => console.log(resultsJSON));
+  }
+
   render() {
+    console.log("SearchTerm: ", this.state.searchTerm);
     return (
       <div className="searchBar">
-        <form className="searchForm">
+        <form className="searchForm" onSubmit={(e) => this.handleSubmit(e)}>
           <label id="searchInputLabel" htmlFor="Search">
             Search
           </label>
@@ -14,6 +48,7 @@ class SearchBar extends Component {
             name="searchInput"
             id="searchInput"
             placeholder="e.g. One Hundred Years Of Solitude"
+            onChange={(e) => this.searchTermChanged(e.target.value)}
           ></input>
           <button type="Submit" id="submitButton">
             Search
